c2115c791bde3c7e2f12f0b7e113ca34cfa3ef65
Anonymous <cryrasinia@proton.me>
2024-11-11 20:46:48 -0700

    [+] Context: Add a list of users to Context

--- include/Context.hpp
+++ include/Context.hpp
@@ -4,10 +4,12 @@
 #include <map>
 
 #include "Client.hpp"
+#include "User.hpp"
 
 class Context
 {
 	std::map<int, Client> *client_list = nullptr;
+	std::vector<User> *user_list = nullptr;
 };
 
 #endif
